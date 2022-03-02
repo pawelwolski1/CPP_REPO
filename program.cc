bb4738b5c14047e9e5eb8211caaa7c78b193c320
pawelwolski1 <95075376+pawelwolski1@users.noreply.github.com>
2022-03-02 12:24:11 +0100

    Pierwszy program

---
+++ program.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int funkcja(){
+
+	int wynik;
+	int A = 1;
+	int B = 2;
+	wynik = A+B;
+	cout << wynik << endl;
+
+	return wynik;
+
+
+}
+
+
+int main(){
+bool a = true;
+int b = 7;
+cout << a + b << endl;
+funkcja();
+return 0;
+}
