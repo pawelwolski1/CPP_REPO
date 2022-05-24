5f51638b8186aa3b76620d4f7084358c59d6f563
pawelwolski1 <95075376+pawelwolski1@users.noreply.github.com>
2022-05-24 21:58:34 +0200

    Add files via upload

---
+++ ZadAdresowaniePamieci.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int a = 13;
+	int b = 21;
+	int c = 39;
+	cout << "Adres a: " << &a << ", wartoœæ a: " << a << endl;
+	cout << "Adres b: " << &b << ", wartoœæ b: " << b << endl;
+	cout << "Adres c: " << &c << ", wartoœæ c: " << c << endl;
+	// Zmienne maj¹ adresy, ró¿ni¹ce siê o rozmiar typu zmiennych.
+	return 0;
+}
