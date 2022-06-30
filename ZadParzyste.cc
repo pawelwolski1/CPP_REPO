f1de000cbe02f23b45f8c8cd5106cc01cb9edf12
pawelwolski1 <95075376+pawelwolski1@users.noreply.github.com>
2022-06-30 11:42:46 +0200

    Add files via upload

---
+++ ZadParzyste.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "ZadParzyste.h"
+
+using namespace std;
+
+void Parzysta::funA(int num){
+	if (num & 1 == 1) cout << "Liczba nieparzysta" << endl;
+	else cout << "Liczba parzysta" << endl;
+}
+
+void Parzysta::funB(int num){
+	if (num % 2 != 0) cout << "Liczba nieparzysta" << endl;
+	else cout << "Liczba parzysta" << endl;
+}
+
+void Parzysta::funC(int num){
+	num % 2 != 0  ? cout << "Liczba nieparzysta" << endl : cout << "Liczba parzysta" << endl;
+}
+
+int Parzysta::main(){
+	int num;
+	cout << "Podaj liczbę całkowitą:" << endl;
+	cin >> num;
+	funB(num);
+	funC(num);
+	return 0;
+}
